063fa4264b0f76e1ac87e7c1ba3a4f97e83ba490
clarencechen <clarencechenct@gmail.com>
2020-11-04 16:54:13 -0800

    condense signup form to one screen

--- public/signup.js
+++ public/signup.js
@@ -1,23 +1,16 @@
 $(document).ready(function() {
 	let user = '';
 	let pass = '';
-	$('#advance').on('click', (event) => {
-		$('#account').toggle(600)
-		$('#background').toggle(600)
-		user = $("#username").val()
-		pass = $('#password').val()
-	});
 	$('#bginfo').submit((e) => {
 		e.preventDefault();
 		const c = $('#ethnicity').find('input')
 			.map((i, el) => $(el).prop('checked') * (1 << i))
-
 		var t = $('#twitter').prop('value')
 		if(t.substring(0, 1) !== "@")
 			t = "@" + t
 		const data = {
-			id: user,
-			pwd: pass,
+			id: $("#username").prop('value'),
+			pwd: $('#password').prop('value'),
 			tw: t,
 			lang: $('#language').prop('value'),
 			dmgs: {
