dd6985058b668a8b7782577c22bae5b634d538de
clarencechen <clarencechenct@gmail.com>
2020-11-08 19:43:45 -0800

    add local camera feed on screen and new controls

---
+++ README.md
@@ -0,0 +1,1 @@
+Source code repository for the Cit-i-zen chat service at https://cit-i-zen.herokuapp.com
--- public/client/video.js
+++ public/client/video.js
@@ -1,17 +1,24 @@
 mediapromise = null
 
-function display(remote) {
-	remote.getVideoTracks()[0].enabled = !$('#novideo').checked
-	remote.getAudioTracks()[0].volume = $('#volume').val
-	remote.getAudioTracks()[0].enabled = !$('#mute').checked
-	$('#novideo').on('input', (e) => {remote.getVideoTracks()[0].enabled = !(e.target.checked)})
+function display(local, remote) {
+	const remoteFeed = $('#remotefeed')[0],
+		localFeed =  $('#localfeed')[0]
+	remoteFeed.srcObject = remote
+	localFeed.srcObject = new MediaStream([local.getVideoTracks()[0]])
+
+	remote.getAudioTracks()[0].enabled = !$('#mute')[0].checked
+	remote.getAudioTracks()[0].volume = $('#volume')[0].value / 100
+	local.getAudioTracks()[0].enabled = !$('#noaudio')[0].checked
+	local.getVideoTracks()[0].enabled = !$('#novideo')[0].checked
+
 	$('#volume').on('input', (e) => {remote.getAudioTracks()[0].volume = e.target.value / 100})
 	$('#mute').on('input', (e) => {remote.getAudioTracks()[0].enabled = !(e.target.checked)})
-
-	const video = document.querySelector('video')
-	video.srcObject = remote
-	video.onloadedmetadata = (e) => {video.play()}
+	$('#noaudio').on('input', (e) => {local.getAudioTracks()[0].enabled = !(e.target.checked)})
+	$('#novideo').on('input', (e) => {local.getVideoTracks()[0].enabled = !(e.target.checked)})
+	remoteFeed.onloadedmetadata = (e) => {remoteFeed.play()}
+	localFeed.onloadedmetadata = (e) => {localFeed.play()}
 }
+
 
 function call(peerid) {
 	mediapromise = mediapromise || navigator.mediaDevices.getUserMedia({audio : true, video : true})
@@ -32,7 +39,7 @@ function call(peerid) {
 		// Cleanup on closure
 		call.on('close', () => {disableCallFeatures(call.peer)})
 		// Display stream when received.
-		call.on('stream', display)
+		call.on('stream', (remote) => {display(stream, remote)})
 		enableCallFeatures(call, stream)
 		let message = $('<div><em>You called ' + call.peer + '.</em></div>').addClass('important')
 		$('#chatbox').append(message)
@@ -42,6 +49,7 @@ function call(peerid) {
 function answer(call) {
 	mediapromise = mediapromise || navigator.mediaDevices.getUserMedia({audio : true, video : true})
 	mediapromise.then((stream) => {
+		// 
 		call.answer(stream)
 		// Handle errors during call.
 		call.on('err', (err) => {
@@ -51,7 +59,7 @@ function answer(call) {
 		// Cleanup on closure
 		call.on('close', () => {disableCallFeatures(call.peer)})
 		// Display stream when received.
-		call.on('stream', display)
+		call.on('stream', (remote) => {display(stream, remote)})
 		enableCallFeatures(call, stream)
 		let message = $('<div><em>' + call.peer + ' called you.</em></div>').addClass('important')
 		$('#chatbox').append(message)
@@ -62,9 +70,7 @@ function disableCallFeatures(peerid) {
 	const video = document.querySelector('video')
 	video.pause()
 	video.srcObject = null
-	$('#novideo').off('input')
-	$('#volume').off('input')
-	$('#mute').off('input')
+	$('.videocontrol').off('input')
 
 	let message = $('<div><em>Your call with ' + peerid + ' has ended.</em></div>').addClass('important')
 	$('#chatbox').append(message)
